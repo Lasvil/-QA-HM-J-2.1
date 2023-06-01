beed91807be7b7c57027c3e4addc822ab7f8e511
ArtyomKiyashko <lounershoargent@gmail.com>
2023-06-01 18:48:03 +0300

    Change Main

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,15 @@ public class Main {
         int deposit = 500;
 
         if (deposit > 1000) {
-            int bonus = deposit/100;
-            System.out.print(balance+deposit+bonus);System.out.println(" Рублей");
-            System.out.print("Из них ");System.out.print(bonus);System.out.println(" Бонусных рублей");
-        }else {
-            System.out.print(balance+deposit);System.out.println(" Рублей");
+            int bonus = deposit / 100;
+            System.out.print(balance + deposit + bonus);
+            System.out.println(" Рублей");
+            System.out.print("Из них ");
+            System.out.print(bonus);
+            System.out.println(" Бонусных рублей");
+        } else {
+            System.out.print(balance + deposit);
+            System.out.println(" Рублей");
             System.out.println("Бонусные рубли отсутствуют");
         }
     }
